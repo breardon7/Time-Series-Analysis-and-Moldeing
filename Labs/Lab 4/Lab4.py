090f3547d323f649373a12679f1bc42e25c19b85
breardon7 <70675472+breardon7@users.noreply.github.com>
2022-02-16 18:49:53 -0500

    lab4

--- Labs/Lab 4/Lab4.py
+++ Labs/Lab 4/Lab4.py
@@ -1,30 +1,38 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 from statistics import variance
 import statsmodels.api as sm
-from scipy.stats import linregress
+from toolbox import autocorrelation_plot
 
-t_train = [112, 118, 132, 129, 121, 134, 148, 163, 119]
+t_train = [112, 118, 132, 129, 121, 134, 148, 136, 119]
 t_test = [104, 118, 115, 126, 141]
 combined = t_train + t_test
+
 
 # question 2
 
 print(combined)
-def average_forecast(original):
-    prediction = []
-    for i in range(len(original)):
-        prediction.append(round((sum(original[:i+1])/(i+1))))
-    return prediction
+def average_forecast(train, test, type):
+    train_forecast = list()
+    test_forecast = list()
+    train_forecast.append(train[0])
+    for i in range(1, len(train) + 1):
+        train_forecast.append(np.mean(train[0:i]))
+    for i in range(0, len(test)):
+        test_forecast.append(train_forecast[-1])
 
-print(average_forecast(t_train))
-print(average_forecast(t_test))
-print(average_forecast(combined))
+    if type == 'train':
+        return train_forecast
+    elif type == 'test':
+        return test_forecast
+
+print('----------------------average---------------------')
+print(average_forecast(t_train, t_test, type='train'))
+print(average_forecast(t_train, t_test, type='test'))
 
 plt.plot(np.arange(1,10), t_train, c='b', label='train')
 plt.plot(np.arange(9,14), t_test, c='r', label='test')
-plt.plot(np.arange(9,14),average_forecast(combined)[-5:], c='g', label='forecast')
+plt.plot(np.arange(9,14), average_forecast(t_train, t_test, type='test'), c='g', label='forecast')
 plt.legend(loc = 'upper left')
 plt.xlabel('Time (t)')
 plt.ylabel('Series Values')
@@ -32,18 +40,6 @@ plt.title('Average Forecast')
 plt.show()
 
 # question 3
-
-def error(original, prediction):
-    error = []
-    for i in range(len(original)):
-        error.append(original[i] - prediction[i])
-    return error
-
-def error_squared(error):
-    error_squared = []
-    for i in range(len(error)):
-        error_squared.append(error[i] ** 2)
-    return error_squared
 
 def MSE(original, prediction):
     error_squared = 0
@@ -53,17 +49,18 @@ def MSE(original, prediction):
     mse = error_squared/len(original)
     return mse
 
-print('average train MSE:', MSE(t_train, average_forecast(t_train)))
-print('average test MSE:', MSE(t_test, average_forecast(t_test)))
+print('average train MSE:', MSE(t_train, average_forecast(t_train, t_test, type='train')))
+print('average test MSE:', MSE(t_test, average_forecast(t_train, t_test, type='test')))
 
 
 # question 4
-
-print('average train variance:', variance(average_forecast(t_train)))
-print('average test variance:', variance(average_forecast(t_test)))
+print('average train mean:', np.mean(average_forecast(t_train, t_test, type = 'train')))
+print('average test mean:', np.mean(average_forecast(t_train, t_test, type = 'test')))
+print('average train variance:', variance(average_forecast(t_train, t_test, type='train')))
+print('average test variance:', variance(average_forecast(t_train, t_test, type='test')))
 
 # question 5
-#print('Q average:', sm.stats.acorr_ljungbox(average_forecast(t_train), lags=[5], return_df=True))
+print('Q average:', sm.stats.acorr_ljungbox(average_forecast(t_train, t_test, type='train'), lags=[5], return_df=True))
 
 # question 6
 
@@ -83,9 +80,15 @@ plt.ylabel('Series Values')
 plt.title('Naive Forecast')
 plt.show()
 
+
+print('----------------------naive---------------------')
+print(naive(t_train))
+print(naive(t_test))
 print('naive train MSE:', MSE(t_train, naive(t_train)))
 print('naive test MSE:', MSE(t_test, np.ones(5)*t_train[-1]))
 
+print('naive train mean:', np.mean(naive(t_train)))
+print('naive test mean:', np.mean(naive(t_test)))
 print('naive train variance:', variance(naive(t_train)))
 print('naive test variance:', variance(np.ones(5)*t_train[-1]))
 
@@ -93,21 +96,114 @@ print('Q naive:', sm.stats.acorr_ljungbox(naive(t_train), lags=[5], return_df=Tr
 
 # question 7
 
-res = linregress(np.arange(1,len(combined)+1), combined)
+def drift(train, test, type):
+    train_forecast = list()
+    test_forecast = list()
+    train_forecast.append(train[0])
+    for i in range(1, len(train) + 1):
+        train_forecast.append(train[i - 1] + (train[i - 1] - train[0]) / i)
+    for i in range(0, len(test)):
+        test_forecast.append(train[-1] + ((train[-1] - train[0]) / (len(train))) * (i + 1))
+
+    if type == 'train':
+        return train_forecast
+    elif type == 'test':
+        return test_forecast
+
+
 
 plt.plot(np.arange(1,10), t_train, c='b', label='train')
 plt.plot(np.arange(9,14), t_test, c='r', label='test')
-plt.plot(np.arange(9,14), t_train[-1] + np.arange(9,14)*res.slope, c='g', label='forecast')
+plt.plot(np.arange(9,14), drift(t_train, t_test, type = 'test'), c='g', label='forecast')
 plt.legend(loc = 'upper left')
 plt.xlabel('Time (t)')
 plt.ylabel('Series Values')
 plt.title('Drift Forecast')
 plt.show()
 
-print('drift train MSE:', MSE(t_train, naive(t_train)))
-print('drift test MSE:', MSE(t_test, np.ones(5)*t_train[-1]))
+print('----------------------drift---------------------')
+print(drift(t_train, t_test, type = 'train'))
+print(drift(t_train, t_test, type = 'test'))
+print('drift train MSE:', MSE(t_train, drift(t_train, t_test, type = 'train')))
+print('drift test MSE:', MSE(t_test, drift(t_train, t_test, type = 'test')))
 
-print('drift train variance:', variance(naive(t_train)))
-print('drift test variance:', variance(np.ones(5)*t_train[-1]))
+print('drift train mean:', np.mean(drift(t_train, t_test, type = 'train')))
+print('drift test mean:', np.mean(drift(t_train, t_test, type = 'test')))
+print('drift train variance:', variance(drift(t_train, t_test, type = 'train')))
+print('drift test variance:', variance(drift(t_train, t_test, type = 'test')))
 
-print('Q drift:', sm.stats.acorr_ljungbox(naive(t_train), lags=[5], return_df=True))
+print('Q drift:', sm.stats.acorr_ljungbox(drift(t_train, t_test, type = 'train'), lags=[5], return_df=True))
+
+# question 8
+
+def ses(train, test, type, alpha=None):
+    train_forecast = list()
+    test_forecast = list()
+
+    if (alpha < 0) or (alpha > 1):
+        raise ValueError('Alpha value has to be integer/float between 0 and 1.')
+    train_forecast.append(train[0])
+    for i in range(1, len(train) + 1):
+        train_forecast.append(alpha * train[i - 1] + (1 - alpha) * train_forecast[i - 1])
+    test_forecast.append(alpha * train[-1] + (1 - alpha) * train_forecast[-1])
+    for i in range(1, len(test)):
+        test_forecast.append(alpha * train[-1] + (1 - alpha) * train_forecast[-1])
+
+    if type == 'train':
+        return train_forecast
+    elif type == 'test':
+        return test_forecast
+
+
+plt.plot(np.arange(1,10), t_train, c='b', label='train')
+plt.plot(np.arange(9,14), t_test, c='r', label='test')
+plt.plot(np.arange(9,14), ses(t_train, t_test, type='test', alpha=0.5), c='g', label='forecast')
+plt.legend(loc = 'upper left')
+plt.xlabel('Time (t)')
+plt.ylabel('Series Values')
+plt.title('SES Forecast')
+plt.show()
+
+print('----------------------ses---------------------')
+print(ses(t_train, t_test, type='train', alpha=0.5))
+print(ses(t_train, t_test, type='test', alpha=0.5))
+print('SES train MSE:', MSE(t_train, ses(t_train, t_test, type='train', alpha=0.5)))
+print('SES test MSE:', MSE(t_test, ses(t_train, t_test, type='test', alpha=0.5)))
+
+print('SES train mean:', np.mean(ses(t_train, t_test, type = 'train', alpha=0.5)))
+print('SES test mean:', np.mean(ses(t_train, t_test, type = 'test', alpha=0.5)))
+print('SES train variance:', variance(ses(t_train, t_test, type='train', alpha=0.5)))
+print('SES test variance:', variance(ses(t_train, t_test, type='test', alpha=0.5)))
+
+print('Q SES:', sm.stats.acorr_ljungbox(ses(t_train, t_test, type='train', alpha=0.5), lags=[5], return_df=True))
+
+# question 9
+fig, ax = plt.subplots(2, 2)
+ax1, ax2, ax3, ax4 = ax.flatten()
+fig.suptitle('SES Forecast')
+ax1.plot(np.arange(1,10), t_train, c='b', label='train')
+ax1.plot(np.arange(9,14), t_test, c='r', label='test')
+ax1.plot(np.arange(9,14), ses(t_train, t_test, type='test', alpha=0), c='g', label='forecast: alpha=0')
+ax1.legend(loc='upper left')
+ax2.plot(np.arange(1,10), t_train, c='b', label='train')
+ax2.plot(np.arange(9,14), t_test, c='r', label='test')
+ax2.plot(np.arange(9,14), ses(t_train, t_test, type='test', alpha=0.25), c='g', label='forecast: alpha=0.25')
+ax2.legend(loc='upper left')
+ax3.plot(np.arange(1,10), t_train, c='b', label='train')
+ax3.plot(np.arange(9,14), t_test, c='r', label='test')
+ax3.plot(np.arange(9,14), ses(t_train, t_test, type='test', alpha=0.75), c='g', label='forecast: alpha=0.75')
+ax3.legend(loc='upper left')
+ax4.plot(np.arange(1,10), t_train, c='b', label='train')
+ax4.plot(np.arange(9,14), t_test, c='r', label='test')
+ax4.plot(np.arange(9,14), ses(t_train, t_test, type='test', alpha=0.99), c='g', label='forecast alpha=0.99')
+ax4.legend(loc='upper left')
+fig.text(0.5, 0.04, 'Time(t)', ha='center')
+fig.text(0.04, 0.5, 'Series Values', va='center', rotation='vertical')
+
+plt.show()
+
+# question 11
+autocorrelation_plot(np.array(average_forecast(t_train, t_test, type='train')), title='ACF Average Prediction', lag=9)
+autocorrelation_plot(np.array(naive(t_train)), title='ACF Naive Prediction', lag=8)
+autocorrelation_plot(np.array(drift(t_train, t_test, type='train')), title='ACF Drift Prediction', lag=9)
+autocorrelation_plot(np.array(ses(t_train, t_test, type='train', alpha=0.5)), title='ACF SES Prediction', lag=9)
--- toolbox.py
+++ toolbox.py
@@ -3,6 +3,8 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.stattools import kpss
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import statsmodels.api as sm
 
 def plot_rolling_mean_var(data, title):
     rolling_var = []
@@ -75,29 +77,121 @@ def autocorrelation(x, lag=1):
     plt.grid(True)
     plt.show()
 
-def average_forecast(original):
-    prediction = []
-    for i in range(len(original)):
-        prediction.append(round((sum(original[:i+1])/(i+1))))
-    return prediction
+def acf_graph_cal(series, lags):
+    ybar = np.mean(series)
+    denom = 0
+    for y in series:
+        denom += (y - ybar) ** 2
+    num = list()
+    # 0 order for ACF is always 1
+    num.append(denom)
+    for i in range(1, lags+1):
+        lag_num = 0
+        for j in range(0, len(series) - i):
+            value = (series[j] - ybar) * (series[j+i] - ybar)
+            lag_num += value
+        num.append(lag_num)
 
-def error(original, prediction):
-    error = []
-    for i in range(len(original)):
-        error.append(original[i] - prediction[i])
-    return error
 
-def error_squared(error):
-    error_squared = []
-    for i in range(len(error)):
-        error_squared.append(error[i] ** 2)
-    return error_squared
+    acf_vals = num / denom
+    rev = acf_vals[::-1][:-1]
+    acf_vals = np.concatenate([rev, acf_vals])
+    integer_range = np.concatenate([np.arange(-1*lags, 0), np.arange(0, lags + 1)])
+    plt.stem(integer_range, acf_vals)
+    plt.fill_between(integer_range,
+                     1.96 / np.sqrt(len(series)), -1.96 / np.sqrt(len(series)),
+                     alpha=0.1, color='red')
+    plt.xlabel('Lags')
+    plt.ylabel('ACF Values')
+    plt.title(f'ACF Plot with Lags of {lags}.')
+    plt.show()
 
-def MSE(original, prediction):
-    error_squared = 0
-    for i in range(len(original)):
-        error = original[i] - prediction[i]
-        error_squared += error ** 2
-    mse = error_squared/len(original)
-    return mse
+    return integer_range, acf_vals
 
+def autocorrelation_plot(x, lag=1, title=''):
+    n = len(x)
+    variance = x.var()
+    x = x - x.mean()
+    r = np.correlate(x, x, mode='full')[-n:]
+    assert np.allclose(r, np.array([(x[:n - k] * x[-(n - k):]).sum() for k in range(n)]))
+    result = r / (variance * (np.arange(n, 0, -1)))
+
+    plt.acorr(result, maxlags = lag)
+    plt.title(title)
+    plt.ylabel('Magnitude')
+    plt.xlabel('Lags')
+    plt.grid(True)
+    plt.show()
+
+
+def forecasting(train, test, type, alpha=None):
+    train_forecast = list()
+    test_forecast = list()
+    if type == 'average':
+        train_forecast.append(train[0])
+        for i in range(1, len(train) + 1):
+            train_forecast.append(np.mean(train[0:i]))
+        for i in range(0, len(test)):
+            test_forecast.append(train_forecast[-1])
+    elif type == 'naive':
+        train_forecast.append(train[0])
+        for i in range(1, len(train) + 1):
+            train_forecast.append(train[i-1])
+        for i in range(0, len(test)):
+            test_forecast.append(train_forecast[-1])
+    elif type == 'drift':
+        train_forecast.append(train[0])
+        for i in range(1, len(train) + 1):
+            train_forecast.append(train[i-1] + (train[i-1] - train[0]) / i)
+        for i in range(0, len(test)):
+            test_forecast.append(train[-1] + ((train[-1] - train[0]) / (len(train))) * (i + 1))
+    elif type == 'ses':
+        if (alpha < 0) or (alpha > 1):
+            raise ValueError('Alpha value has to be integer/float between 0 and 1.')
+        train_forecast.append(train[0])
+        for i in range(1, len(train) + 1):
+            train_forecast.append(alpha * train[i-1] + (1-alpha) * train_forecast[i-1])
+        test_forecast.append(alpha * train[-1] + (1-alpha) * train_forecast[-1])
+        for i in range(1, len(test)):
+            test_forecast.append(alpha * train[-1] + (1-alpha) * train_forecast[-1])
+    else:
+        raise ValueError(f'Value of {type} is not a valid value for type variable.')
+
+    train_error = [a - b for a, b in zip(train_forecast, train)]
+    test_error = [a - b for a, b in zip(test_forecast, test)]
+
+
+    train_labels = [x + 1 for x in list(range(0, len(train)))]
+    test_labels = [x + max(train_labels) for x in list(range(0, len(test)))]
+    plt.plot(train_labels, train, c='blue', label='train')
+    plt.plot(test_labels, test, c='orange', label='test')
+    plt.plot(test_labels, test_forecast, c='green', label='forecast')
+    plt.xlabel('Time (t)')
+    plt.ylabel('Series Value')
+    #plt.legend([train, test, test_forecast], ['train', 'test', 'forecast'])
+    plt.legend()
+    if type == 'ses':
+        plt.title(f'Time Series Value and Prediction using {type} method and alpha of {alpha}')
+    else:
+        plt.title(f'Time Series Value and Prediction using {type} method')
+    plt.show()
+
+    # Plot acf of errors
+    acf_graph_cal(train_error, lags=5)
+
+    print(f'The train values are {train}')
+    print(f'The train predictions are {train_forecast}')
+    print(f'The train error is {train_error}')
+    print(f'The train squared error is {np.square(train_error)}')
+    print(f'The train MSE is {np.mean(np.square(train_error))}')
+    print(f'The train error variance is {np.var(train_error)}')
+    print(f'The Q values for the training set are {sm.stats.acorr_ljungbox(train_forecast, lags=5)}')
+    print(f'The test values are {test}')
+    print(f'The test forecasts are {test_forecast}')
+    print(f'The test error is {test_error}')
+    print(f'The test squared error is {np.square(test_error)}')
+    print(f'The test MSE is {np.mean(np.square(test_error))}')
+    print(f'The test error variance is {np.var(test_error)}')
+
+    #return train, test, train_forecast, test_forecast
+    return None
